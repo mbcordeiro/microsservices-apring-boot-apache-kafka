6d226f7c193e152d28bdb6c21b955f0652370492
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-09-30 23:14:07 -0300

    Add car store api

---
+++ car-store-api/src/main/java/com/matheuscordeiro/carstoreapi/CarStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.carstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarStoreApiApplication.class, args);
+	}
+
+}
---
+++ car-store-api/src/test/java/com/matheuscordeiro/carstoreapi/CarStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.carstoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
