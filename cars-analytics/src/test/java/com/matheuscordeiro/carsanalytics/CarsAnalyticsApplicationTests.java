5eae5569ad8ea3bd5ca7b83832dfabb18c886f30
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-10-01 16:25:14 -0300

    Add Cars analitics

---
+++ cars-analytics/src/test/java/com/matheuscordeiro/carsanalytics/CarsAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.carsanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
